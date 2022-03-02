e5cf7b71bf5ac5b0708a37f35e94b1a47681ba23
Chris Tampakakis <tampakc@yahoo.gr>
2022-03-02 21:58:23 +0200

    Phased out POJOs, implemented timestamps using Dates

--- src/main/java/infoproject/ActiveMQRead.java
+++ src/main/java/infoproject/ActiveMQRead.java
@@ -27,21 +27,29 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.api.windowing.windows.Window;
 import org.apache.flink.streaming.connectors.activemq.AMQSource;
 import org.apache.flink.streaming.connectors.activemq.AMQSourceConfig;
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
 import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
+import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 import org.w3c.dom.TypeInfo;
 //import org.apache.flink.streaming.api.scala.DataStream;
@@ -54,7 +62,7 @@ import java.text.SimpleDateFormat;
 
 public class ActiveMQRead {
 
-	static final Time windowTime = Time.seconds(10);
+	static final Time windowTime = Time.minutes(2);
 
 
 	public static void main(String[] args) throws Exception {
@@ -107,7 +115,33 @@ public class ActiveMQRead {
 
 		final OutputTag<Tuple2<Long, Integer>> lateOutputTag = new OutputTag<Tuple2<Long, Integer>>("late-data"){};
 
-		/*SingleOutputStreamOperator<DailyData> alerts = input
+		SingleOutputStreamOperator<Tuple5<Integer, Integer, Integer, Double, Date>> alerts = input.
+				windowAll(TumblingEventTimeWindows.of(windowTime)).
+				sideOutputLateData(lateOutputTag).
+				apply(new AllWindowFunction<Tuple2<Long, Integer>, Tuple5<Integer, Integer, Integer, Integer, Date>, TimeWindow>() {
+						  @Override
+						  public void apply(TimeWindow timeWindow, Iterable<Tuple2<Long, Integer>> iterable, Collector<Tuple5<Integer, Integer, Integer, Integer, Date>> collector) throws Exception {
+							  for(Tuple2<Long, Integer> in : iterable)
+								  collector.collect(new Tuple5<>(in.f1, in.f1, in.f1, 1, new Date(timeWindow.getStart())));
+						  }
+					  }
+				).
+				keyBy(4).
+				reduce(new ReduceFunction<Tuple5<Integer, Integer, Integer, Integer, Date>>() {
+					@Override
+					public Tuple5<Integer, Integer, Integer, Integer, Date> reduce(Tuple5<Integer, Integer, Integer, Integer, Date> t1, Tuple5<Integer, Integer, Integer, Integer, Date> t2) throws Exception {
+						return new Tuple5<>(t1.f0 + t2.f0, Math.min(t1.f1, t2.f1), Math.max(t1.f2, t2.f2), t1.f3 + t2.f3, t1.f4);
+					}
+				}).
+				map(new MapFunction<Tuple5<Integer, Integer, Integer, Integer, Date>, Tuple5<Integer, Integer, Integer, Double, Date>>() {
+					@Override
+					public Tuple5<Integer, Integer, Integer, Double, Date> map(Tuple5<Integer, Integer, Integer, Integer, Date> in) throws Exception {
+						return new Tuple5<>(in.f0, in.f1, in.f2, in.f0*1.0/in.f3, in.f4);
+					}
+				});
+
+
+		/*SingleOutputStreamOperator<Tuple5<Integer, Integer, Integer, Double, Date> alerts = input
 				//.keyBy(Tuple2<Long, Integer>::getKey)
 				.windowAll(TumblingEventTimeWindows.of(windowTime))
 				//.sideOutputLateData(lateOutputTag)
@@ -141,36 +175,26 @@ public class ActiveMQRead {
 
 		input.addSink(new PrintSinkFunction<>());
 
-		/*CassandraSink.addSink(alerts)
-				//.setQuery("INSERT INTO infosystems.daily (sum, min, max, avg, day) VALUES (?, ?, ?, ?, ?)")
+		CassandraSink.addSink(alerts)
+				.setQuery("INSERT INTO infosystems.daily (sum, min, max, avg, window) VALUES (?, ?, ?, ?, ?)")
 				.setHost("localhost")
-				.build();*/
+				.build();
 
-		/*DataStream<LateEvent> late = alerts
+		DataStream<Tuple2<Date, Integer>> late = alerts
 				.getSideOutput(lateOutputTag)
-				.map(new MapFunction<Tuple2<Long, Integer>, LateEvent>() {
+				.map(new MapFunction<Tuple2<Long, Integer>, Tuple2<Date, Integer>>() {
 					@Override
-					public LateEvent map(Tuple2<Long, Integer> in) throws Exception {
-						return new LateEvent(in.f0, in.f1);
+					public Tuple2<Date, Integer> map(Tuple2<Long, Integer> in) throws Exception {
+						return new Tuple2<>(new Date(in.f0), in.f1);
 					}
 				});
 
-		late.addSink(new PrintSinkFunction<>());*/
+		late.addSink(new PrintSinkFunction<>());
 
-		/*CassandraSink.addSink(late)
-				//.setQuery("INSERT INTO infosystems.late (time, value) VALUES (?, ?);")
-				//.setHost("localhost")
-				.setClusterBuilder(
-						new ClusterBuilder() {
-							@Override
-							protected Cluster buildCluster(Cluster.Builder builder) {
-								return builder.addContactPoint("127.0.0.1").build();
-							}
-						})
-				.setMapperOptions(() -> new Mapper.Option[]{Mapper.Option.saveNullFields(true)})
+		CassandraSink.addSink(late)
+				.setQuery("INSERT INTO infosystems.late (time, value) VALUES (?, ?);")
+				.setHost("localhost")
 				.build();
-
-		 */
 
 
 		/*DataStream<Tuple5<LocalDate, Integer, Integer, Integer, Double>> test = input.map(new MapFunction<Tuple2<Long, Integer>, Tuple5<LocalDate, Integer, Integer, Integer, Double>>() {
